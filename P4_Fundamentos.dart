30df7a7133c26e8c53e421b69b6effa25d29da5a
CA12REZ <111254182+CA12REZ@users.noreply.github.com>
2024-09-18 13:43:53 -0600

    practica5

---
+++ P4_Fundamentos.dart
@@ -0,0 +1,27 @@
+void main (){
+  //parctica 04: tipo de dato DART
+  
+  //Cadenas (String)
+  
+ final String pokemon = 'Picachu';
+  
+  //Entero
+  final int hp =100;
+  
+  //boolean
+  final bool isAlive = true;
+  
+  //Lista
+  
+  final abilities = ['Impact-trueno','Cola de hierro','Ataque Rapido'];
+  final sprites = <String>['Picachu_front.png','Pikachu_back.png'];
+  
+  print ("""El pokemon que elegiste es : $pokemon
+  ----------------------------------------------------------------------------------------
+  Las estadisticas de $pokemon son :
+  Vida (HP) :$hp
+  Estatus de Vida : $isAlive
+  Habilidades:$abilities
+  Imagenes:$sprites
+  ----);
+}
