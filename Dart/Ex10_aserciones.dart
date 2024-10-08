c3f576da67a11d7f6c1c996078cf1d9f01b9f09b
CA12REZ <210458@utxicotepec.edu.mx>
2024-10-08 10:25:35 -0600

    Practica12

---
+++ Dart/Ex10_aserciones.dart
@@ -0,0 +1,24 @@
+void main(){
+  final mySquare = Square(side:10);
+  mySquare.side = -5;
+  print("El area es igual a: ${mySquare.area}");
+}
+class Square {
+  double _side;
+
+  Square({ required double side })
+    : assert(side >= 0, 'Side mosst be >=0'),
+    _side = side;
+
+  double get area {
+      return _side * _side;
+  }
+  set side(double value){
+    print("setting new value $value");
+      if (value < 0) throw 'Value must be >=0';
+    _side = value;
+  }
+  double calculateArea() {
+  return _side * _side;
+  }
+}
